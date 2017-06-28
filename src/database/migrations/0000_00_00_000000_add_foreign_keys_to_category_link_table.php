aca4a316ee0e6fa431b4fb60169ba106d43e8f5f
Alexandr Fanamurov <fanamura@gmail.com>
2017-06-28 12:27:58 +1000

    Fix migrations

--- src/database/migrations/0000_00_00_000000_add_foreign_keys_to_category_link_table.php
+++ src/database/migrations/0000_00_00_000000_add_foreign_keys_to_category_link_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class AddForeignKeysToLarrockCategoryLinkTable extends Migration {
+class AddForeignKeysToCategoryLinkTable extends Migration {
 
 	/**
 	 * Run the migrations.
--- src/database/migrations/0000_00_00_000000_create_category_link_table.php
+++ src/database/migrations/0000_00_00_000000_create_category_link_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateLarrockCategoryLinkTable extends Migration {
+class CreateCategoryLinkTable extends Migration {
 
 	/**
 	 * Run the migrations.
