07d16507d8f0029253907b9385eb65a0042afead
Alexandr Fanamurov <fanamura@gmail.com>
2017-06-28 13:25:02 +1000

    Fix migrations

--- src/LarrockComponentCategoryServiceProvider.php
+++ src/LarrockComponentCategoryServiceProvider.php
@@ -29,21 +29,19 @@ class LarrockComponentCategoryServiceProvider extends ServiceProvider
         $timestamp = date('Y_m_d_His', time());
         $migrations = [];
         if ( !class_exists('CreateCategoryTable')){
-            $migrations = [__DIR__.'/../database/migrations/0000_00_00_000000_create_category_table.php' => database_path('migrations/'.$timestamp.'_create_category_table.php')];
+            $migrations[__DIR__.'/../database/migrations/0000_00_00_000000_create_category_table.php'] = database_path('migrations/'.$timestamp.'_create_category_table.php');
         }
         if ( !class_exists('CreateCategoryLinkTable')){
-            $migrations = [__DIR__.'/../database/migrations/0000_00_00_000000_create_category_link_table.php' => database_path('migrations/'.$timestamp.'_create_category_link_table.php')];
+            $migrations[__DIR__.'/../database/migrations/0000_00_00_000000_create_category_link_table.php'] = database_path('migrations/'.$timestamp.'_create_category_link_table.php');
         }
         if ( !class_exists('AddForeignKeysToCategoryTable')){
-            $migrations = [__DIR__.'/../database/migrations/0000_00_00_000000_add_foreign_keys_to_category_table.php' => database_path('migrations/'.$timestamp.'_add_foreign_keys_to_category_table.php')];
+            $migrations[__DIR__.'/../database/migrations/0000_00_00_000000_add_foreign_keys_to_category_table.php'] = database_path('migrations/'.$timestamp.'_add_foreign_keys_to_category_table.php');
         }
         if ( !class_exists('AddForeignKeysToCategoryLinkTable')){
-            $migrations = [__DIR__.'/../database/migrations/0000_00_00_000000_add_foreign_keys_to_category_link_table.php' => database_path('migrations/'.$timestamp.'_add_foreign_keys_to_category_link_table.php')];
+            $migrations[__DIR__.'/../database/migrations/0000_00_00_000000_add_foreign_keys_to_category_link_table.php'] = database_path('migrations/'.$timestamp.'_add_foreign_keys_to_category_link_table.php');
         }
 
-        $this->publishes([
-            $migrations
-        ], 'migrations');
+        $this->publishes($migrations, 'migrations');
 
         $this->publishes([
             __DIR__.'/views' => base_path('resources/views/larrock')
