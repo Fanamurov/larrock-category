f4c421649a18f4524926eedbff8cee100632c184
Alexandr Fanamurov <fanamura@gmail.com>
2017-07-06 13:29:25 +1000

    Fix

--- src/routes.php
+++ src/routes.php
@@ -2,7 +2,7 @@
 
 use Larrock\ComponentCategory\AdminCategoryController;
 
-Route::group(['prefix' => 'admin', 'middleware'=> ['web', 'level:2', 'LarrockAdminMenu']], function(){
+Route::group(['prefix' => 'admin', 'middleware'=> ['web', 'level:2', 'LarrockAdminMenu', 'SaveAdminPluginsData']], function(){
     Route::resource('category', AdminCategoryController::class);
     Route::post('/category/storeEasy', AdminCategoryController::class .'@storeEasy');
 });
