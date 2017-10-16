cf8d608bf26874e03067c3e4b1a713da978e32ff
Alexandr Fanamurov <fanamura@gmail.com>
2017-10-16 14:37:28 +1000

    update

--- src/LarrockComponentCategoryServiceProvider.php
+++ src/LarrockComponentCategoryServiceProvider.php
@@ -16,6 +16,8 @@ class LarrockComponentCategoryServiceProvider extends ServiceProvider
         $this->loadRoutesFrom(__DIR__.'/routes.php');
 
         $this->loadViewsFrom(__DIR__.'/views', 'larrock');
+
+        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
     }
 
     /**
@@ -29,24 +31,6 @@ class LarrockComponentCategoryServiceProvider extends ServiceProvider
             $class = config('larrock.components.category', CategoryComponent::class);
             return new $class;
         });
-
-        $timestamp = date('Y_m_d_His', time());
-        $timestamp_after = date('Y_m_d_His', time()+10);
-        $migrations = [];
-        if ( !class_exists('CreateCategoryTable')){
-            $migrations[__DIR__.'/database/migrations/0000_00_00_000000_create_category_table.php'] = database_path('migrations/'.$timestamp.'_create_category_table.php');
-        }
-        if ( !class_exists('CreateCategoryLinkTable')){
-            $migrations[__DIR__.'/database/migrations/0000_00_00_000000_create_category_link_table.php'] = database_path('migrations/'.$timestamp.'_create_category_link_table.php');
-        }
-        if ( !class_exists('AddForeignKeysToCategoryTable')){
-            $migrations[__DIR__.'/database/migrations/0000_00_00_000000_add_foreign_keys_to_category_table.php'] = database_path('migrations/'.$timestamp_after.'_add_foreign_keys_to_category_table.php');
-        }
-        if ( !class_exists('AddForeignKeysToCategoryLinkTable')){
-            $migrations[__DIR__.'/database/migrations/0000_00_00_000000_add_foreign_keys_to_category_link_table.php'] = database_path('migrations/'.$timestamp_after.'_add_foreign_keys_to_category_link_table.php');
-        }
-
-        $this->publishes($migrations, 'migrations');
 
         $this->publishes([
             __DIR__.'/views' => base_path('resources/views/vendor/larrock')
