458f41e02c91ee61e206e77a8c84dddc372be512
Alexandr Fanamurov <fanamura@gmail.com>
2017-10-31 15:09:32 +1000

    +searchSiteAdmin Middleware

--- src/routes.php
+++ src/routes.php
@@ -2,7 +2,7 @@
 
 use Larrock\ComponentCategory\AdminCategoryController;
 
-Route::group(['prefix' => 'admin', 'middleware'=> ['web', 'level:2', 'LarrockAdminMenu', 'SaveAdminPluginsData']], function(){
+Route::group(['prefix' => 'admin', 'middleware'=> ['web', 'level:2', 'LarrockAdminMenu', 'SaveAdminPluginsData', 'SiteSearchAdmin']], function(){
     Route::resource('category', AdminCategoryController::class);
     Route::post('/category/storeEasy', AdminCategoryController::class .'@storeEasy');
 });
