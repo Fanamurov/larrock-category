9193b82cff933650b953ff84965eeef5f1077bb2
Alexandr Fanamurov <fanamurov@ya.ru>
2018-04-06 13:44:22 +1000

    fix include-create-easy template

--- views/admin/category/include-create-easy.blade.php
+++ views/admin/category/include-create-easy.blade.php
@@ -1,6 +1,7 @@
 {{-- Создание раздела --}}
 <tr class="create-category uk-hidden">
     <form action="/admin/category/storeEasy" method="post" class="uk-form">
+        <td></td>
         <td>
             <div class="create-category-title_div uk-form">
                 <input type="text" placeholder="Название раздела" class="uk-form-controls create-category-title uk-width-1-1 uk-input" name="title">
@@ -10,7 +11,7 @@
             <input type="text" name="position" value="0" class="uk-form-controls uk-input uk-form-width-small"
                    data-toggle="tooltip" data-placement="bottom" title="Вес. Чем больше, тем выше в списках">
         </td>
-        <td>
+        <td colspan="3">
             {!! csrf_field() !!}
             @if($parent && $parent > 0)
                 <input type="hidden" name="parent" value="{{ $parent }}">
